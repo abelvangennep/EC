f78d46f2dec54521a71210b7c48ab4feb6c8a80a
abelvangennep <abel1998@gmail.com>
2022-09-20 14:21:20 +0200

    Link insertion added

--- evoman_framework_RL/NEAT_mutate.py
+++ evoman_framework_RL/NEAT_mutate.py
@@ -27,5 +27,28 @@ def add_node(individual, id_node, highest_innov_id):
     
     return individual
 
-def link_insertion(individual):
+def link_insertion(individual, id_node, highest_innov_id):
+    hidden_nodes = []
+    all_nodes = {}
+    for connection in individual.get_network():
+        if connection.in_node.type == "Hidden":
+            hidden_nodes.append(connection.in_node)
+
+        if not connection.in_node == "Hidden":
+            all_nodes.add(connection.in_node)
+        if not connection.out_node == "Hidden": 
+            all_nodes.add(connection.out_node)
+
+    if hidden_nodes:
+        selected_node = random.choice(hidden_nodes)
+        all_nodes.remove(selected_node)
+        connected_node = random.choice(all_nodes)
+        weight = random.uniform(-1,1)
+        if connected_node.type == "Input":
+            individual.add_connection(connected_node, selected_node, weight, id_node, highest_innov_id)
+        elif connected_node.type == "Output":
+            individual.add_connection(selected_node, connected_node, weight, id_node, highest_innov_id)
+        else:
+            print("SHOULD NOT BE HERE, PROBLEM IN NEAT_MUTATION")
+
     return individual
