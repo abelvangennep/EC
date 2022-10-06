ff412433ba7aa55e51b0759b3f93f757b05d487d
TimvL88 <tim.von.luecken@gmx.de>
2022-10-06 12:33:19 +0200

    Neat_mutate.py

--- EA_2/NEAT_mutate.py
+++ EA_2/NEAT_mutate.py
@@ -5,17 +5,19 @@ from NEAT import *
 
 def mutate(individual, mutation_prob):
     for node in individual.get_network():
-        weight = node.get_weight()
-        if weight > mutation_prob:
-            # replace with other random weights
-            node.set_weight(np.random.uniform(-1,1))
-
-        if weight < mutation_prob:
-            # add/subtract random values
-            node.set_weight(weight + np.random.uniform(-1,1))
-
-        if weight == mutation_prob:
-            # keep the weight
-            node.set_weight(weight)
+        r = np.random.uniform(0, 1)
+        if r > 1 - mutation_prob:
+            a = np.random.uniform(0, 1)
+            if a<1/3:
+                # replace with other random weights
+                node.set_weight(np.random.uniform(-1,1))
+            elif a>2/3:
+                node.set_weight(weight + np.random.uniform(-1, 1))
+            else:
+                # switch weights
+                switch = random.randint(0,len(individual.get_network()-1))
+                temp = individual.get_network()[switch].get_weight()
+                individual.get_network()[switch].set_weight(node.get_weight())
+                node.set_weight(temp)
     return individual
 
