896ed8ceb1025e4a3bef264818e976def2f3f4ea
TimvL88 <tim.von.luecken@gmx.de>
2022-10-06 10:59:09 +0200

    NEAT_mutate.py EA2

--- EA_2/NEAT_mutate.py
+++ EA_2/NEAT_mutate.py
@@ -1,63 +1,12 @@
 import random
 import numpy as np
 
-def mutate(individual, id_node, highest_innov_id, weight_mutation_lambda = 3, link_insertion_lambda=.05, node_insertion_lambda=.05):
-    mut_string = 0
-    for _ in range(np.random.poisson(weight_mutation_lambda, 1)[0]):
-        individual = adjust_weight(individual)
-        mut_string = mut_string+100
-
-    for _ in range(np.random.poisson(link_insertion_lambda, 1)[0]):
-        individual =  link_insertion(individual, id_node, highest_innov_id)
-        id_node +=1
-        highest_innov_id += 1
-        mut_string = mut_string + 20
-
-    for _ in range(np.random.poisson(node_insertion_lambda, 1)[0]):
-        individual = add_node(individual, id_node, highest_innov_id)
-        id_node +=1
-        highest_innov_id += 2
-        mut_string = mut_string + 3
-
-    return individual, id_node, highest_innov_id, mut_string
-
-def adjust_weight(individual):
-    network = individual.get_network()
-    for i in range(random.randint(1,10)):
-        single_connection = random.choice(network)
-        single_connection.weight = random.uniform(-1,1)
-
-    return individual
-
-def add_node(individual, id_node, highest_innov_id):
-    network = individual.get_network()
-    single_connection = random.choice(network)
-    single_connection.disable()
-    individual.add_connection(single_connection.inn, single_connection.out, single_connection.weight, id_node, highest_innov_id)
-    
-    return individual
-
-def link_insertion(individual, id_node, highest_innov_id):
-    hidden_nodes = []
-    all_nodes = set()
-    for connection in individual.get_network():
-        if connection.inn.type == "Hidden":
-            hidden_nodes.append(connection.inn)
-
-        if not connection.inn == "Hidden":
-            all_nodes.add(connection.inn)
-        if not connection.out == "Hidden":
-            all_nodes.add(connection.out)
-
-    if hidden_nodes:
-        selected_node = random.choice(hidden_nodes)
-        all_nodes.remove(selected_node)
-        connected_node = random.sample(all_nodes, 1)[0]
-        weight = random.uniform(-1, 1)
-        if connected_node.type == "Input":
-            individual.add_connection(connected_node, selected_node, weight, id_node, highest_innov_id)
-        elif connected_node.type == "Output":
-            individual.add_connection(selected_node, connected_node, weight, id_node, highest_innov_id)
-
-
+def mutate(individual, mutation_prob):
+    for n in individual.get_network():
+        r = np.random.uniform(0,1)
+        if n > mutation_prob:
+            n.set_weight(np.random.uniform(-1,1))
+            #switch with other weight
+            #replace by new weight
+            #add random value to current weight
     return individual
