b221bf6f554e6587d1c5d2405b510a041805f59d
TimvL88 <tim.von.luecken@gmx.de>
2022-10-12 18:34:54 +0200

    EA2 final.py

--- EA_2/controller_generalist_nn_final.py
+++ EA_2/controller_generalist_nn_final.py
@@ -130,27 +130,31 @@ def run_neat(list_):
         print('Generation ', gen, ' took ', time.time()-start_gen, ' seconds to elapse. Highest fitness value was ', np.max(fitnesses), 'lowest enemy life: ',np.min(enemy_life) )
     
     best_num_wins = 0
-    best_fitness = 0
+    best_fitness = -50
     best_individual = None
-
+    best_ind_gains = []
     for good_individual in best_individuals:
         enemy_win = []
         fitness_all_enemies = 0
-
+        gains = []
         for enem in range(1,9):
             env2.update_parameter('enemies', [enem])
             f, p, e, t = env2.play(pcont=good_individual[1][0:265])
             enemy_win.append(e==0)
             fitness_all_enemies+=f
+            gains.append(p-e)
 
-        if sum(enemy_win) > best_num_wins or (sum(enemy_win) == best_num_wins and fitness_all_enemies > best_fitness):   
-            best_individual =  good_individual[1]
+        if sum(enemy_win) > best_num_wins or (sum(enemy_win) == best_num_wins and fitness_all_enemies > best_fitness):
+
+            best_individual = good_individual[1]
+            #print(best_individual, ' shape: ', len(best_individual))
             best_num_wins = sum(enemy_win)
             best_fitness = good_individual[0]
+            best_ind_gains = gains
 
         print('Test against all enemies; won enemies: ', enemy_win, 'Best num of wins', best_num_wins, ' fitness 8 enemies sum: ', fitness_all_enemies)
         
-    return overview, best_fitness, best_individual
+    return overview, best_fitness, best_individual, best_ind_gains
 
 
 
@@ -158,7 +162,9 @@ def final_experiment_data(runs = 10, number_generations = 20, population_size = 
     "Writes the best outcomes to a seperate csv-file"
     plot_max_fit = np.zeros((number_generations,runs))
     plot_mean_fit = np.zeros((number_generations,runs))
+    best_ind_gains = np.zeros((8, runs))
     scores_of_best_individuals = []
+
     for i in range(int(runs/2)):
         with concurrent.futures.ProcessPoolExecutor() as executor:
             results = executor.map(run_neat, [[number_generations, population_size, tournament_size, sigma] for _ in range(2)])
@@ -168,10 +174,13 @@ def final_experiment_data(runs = 10, number_generations = 20, population_size = 
         for index, new_cols in enumerate(results):
             overview = new_cols[0]
             best_fitness= new_cols[1]
-            np.savetxt('best_ind/'+str(index)+'.txt', new_cols[2][:,0:265])
+            #print('new_cols 2', new_cols[2][0:265], ' länge: ', len(new_cols[2][0:265]))
+            np.savetxt('best_ind/'+str(index+2*i)+'.txt', new_cols[2][0:265])
+            ind_gains = new_cols[3]
             scores_of_best_individuals.append(best_fitness)
             plot_mean_fit[:,i*2+index] = overview[:,0]
             plot_max_fit[:,i*2+index] = overview[:,1]
+            best_ind_gains[:, i * 2 + index] = ind_gains
     
     print(scores_of_best_individuals)
     df_boxplot = pd.DataFrame(scores_of_best_individuals)
@@ -184,6 +193,9 @@ def final_experiment_data(runs = 10, number_generations = 20, population_size = 
     df_mean_fit = pd.DataFrame(plot_mean_fit)
     df_mean_fit.to_csv('mean_fitness_EA2'+str(runs)+'runs_enemy'+str(enemies[0])+'.csv', index_label=None)
 
+    df_ind_gains = pd.DataFrame(best_ind_gains)
+    df_ind_gains.to_csv('best_individual_gains_EA2'+str(runs)+'runs_enemy_group'+str(enemies[0])+'.csv', index_label=None)
+
 if __name__ == '__main__':
-    final_experiment_data(runs = 2, number_generations = 1, population_size = 20,  tournament_size = 3, mutation_prob = 0.3, enemies=enemies, sigma=0.1) #runs has to be even number
+    final_experiment_data(runs = 4, number_generations = 1, population_size = 20,  tournament_size = 3, mutation_prob = 0.3, enemies=enemies, sigma=0.1) #runs has to be even number
 
